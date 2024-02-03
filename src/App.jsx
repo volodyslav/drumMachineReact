410b40e4d9b0daa623161b0ae37336eceadb83a1
Volodyslav <122315136+volodyslav@users.noreply.github.com>
2024-02-03 17:58:25 +0200

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,64 @@
-import React from 'react'
+import { useState } from "react"
 
 const App = () => {
+  const [display, setDisplay] = useState("")
+
+  const buttons = [
+    {
+      id: 0,
+      title: "Q",
+      audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+    },
+    {
+      id: 1,
+      title: "W",
+      audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+    },
+    {
+      id: 2,
+      title: "E",
+      audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+    },
+    {
+      id: 3,
+      title: "A",
+      audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+    },
+    {
+      id: 4,
+      title: "S",
+      audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+    },
+    {
+      id: 5,
+      title: "D",
+      audio: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+    },
+    {
+      id: 6,
+      title: "Z",
+      audio: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+    },
+    {
+      id: 7,
+      title: "X",
+      audio: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+    },
+    {
+      id: 8,
+      title: "D",
+      audio: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+    }
+  ]
+
   return (
-    <div id="drum-machine" className='border-4 bg-slate-900 text-white border-green-900 p-10 rounded-lg'>
-      <div id="display"></div>
-      <div>
-        <button className='drum-pad'>Q <audio src='./sounds/Heater-6.mp3' id='Q'></audio></button>
-        <button className='drum-pad'>W <audio src='./sounds/Heater-1.mp3' id='W'></audio></button>
-        <button className='drum-pad'>E <audio src='./sounds/Heater-2.mp3' id='E'></audio></button>
-        <button className='drum-pad'>A <audio src='./sounds/Heater-3.mp3' id='A'></audio></button>
-        <button className='drum-pad'>S <audio src='./sounds/Heater-4_1.mp3' id='S'></audio></button>
-        <button className='drum-pad'>D <audio src='./sounds/Cev_H2.mp3' id='D'></audio></button>
-        <button className='drum-pad'>Z <audio src='./sounds/Dsc_Oh.mp3' id='Z'></audio></button>
-        <button className='drum-pad'>X <audio src='./sounds/Kick_n_Hat.mp3' id='X'></audio></button>
-        <button className='drum-pad'>C <audio src='./sounds/RP4_KICK_1.mp3' id='C'></audio></button>
+    <div id="drum-machine" className='border-4 shadow-lg flex shadow-black space-x-4 bg-slate-900 text-white border-green-900 p-10 rounded-lg'>
+      <div className=' grid grid-cols-3'>
+      {buttons.map(button => (
+        <button key={button.id} className='drum-pad' ><audio src={button.audio} id={button.title}></audio>{button.title}</button>
+      ))}
       </div>
+      <div id="display" className=' border border-gray-600 p-4 mt-2 bg-white rounded-lg min-w-40 h-10 text-black '>{display}</div>
     </div>
   )
 }
